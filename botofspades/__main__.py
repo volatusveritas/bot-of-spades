17f2b245b69928bd266c2095cfa3666801d9c4f3
Volatus <volatusCoder@outlook.com>
2022-07-07 13:14:13 -0300

    Generalized the bot

--- botofspades/__main__.py
+++ botofspades/__main__.py
@@ -1,14 +1,14 @@
 from discord.ext import commands
 
-from shutsubot import constants
+from botofspades import constants
 
 
-class Shutsubot(commands.Bot):
-    def load_default_exts(self):
+class BotOfSpades(commands.Bot):
+    def load_default_exts(self) -> None:
         for ext in constants.DEFAULT_EXTENSIONS:
             self.load_extension(f"shutsubot.extensions.{ext}")
 
 
-bot = Shutsubot(constants.PREFIXES)
+bot: BotOfSpades = BotOfSpades(constants.PREFIXES)
 bot.load_default_exts()
 bot.run(constants.TOKEN)
--- botofspades/constants.py
+++ botofspades/constants.py
@@ -1,7 +1,5 @@
 TOKEN: str = (
     'OTcyMjIwNDg3ODMwNzYxNTUz.GDEtSZ.riuy2kG455BYyRrtnR4RVu94l8XNvO1pJRcDJo'
 )
-PREFIXES: list[str] = ["shutsu", "sb.", "s."]
-DEFAULT_EXTENSIONS: list[str] = [
-    "dice",
-]
+PREFIXES: list[str] = ["spades", "bs.", "&"]
+DEFAULT_EXTENSIONS: list[str] = []
